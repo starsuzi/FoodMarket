1da2146532b729af1d52d893547b3af2db8f0733
sy <starsuzi@naver.com>
2018-07-25 18:03:25 +0900

    gongutable file 추가

---
+++ foodPrototype/GongGuDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GongGuDetailTableViewCell.swift
+//  foodPrototype
+//
+//  Created by 방문사용자 on 2018. 7. 25..
+//  Copyright © 2018년 송. All rights reserved.
+//
+
+import UIKit
+
+class GongGuDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
